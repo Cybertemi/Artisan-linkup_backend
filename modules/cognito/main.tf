3b5563d1d921d1618f8cd6c02bc0916c788fd218
Temitope Ilori <iloritemitope1999@gmail.com>
2025-08-24 20:59:37 +0100

    fixes

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -106,7 +106,7 @@ resource "aws_cognito_user_pool" "congito_end_user_userpool" {
 
 # creates user pool domain link 
 resource "aws_cognito_user_pool_domain" "main" {
-  domain       = "${var.RESOURCE_PREFIX}-112233"
+  domain       = "${var.RESOURCE_PREFIX}-112244"
   user_pool_id = aws_cognito_user_pool.congito_end_user_userpool.id
 }
 
